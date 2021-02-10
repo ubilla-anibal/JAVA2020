7e6aa7ab3f3ce45113fd88be96c9762f5a1156e0
AnibalUbilla <ubilla.anibal@gmail.com>
2021-02-10 17:00:00 +0100

    Added HelloWord

---
+++ HelloWorld0/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String welMsg = "Hello, World!";
+        System.out.println(welMsg);
+    }
+}
